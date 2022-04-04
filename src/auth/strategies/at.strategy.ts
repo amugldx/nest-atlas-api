d7fda0a76515cb498c66addd2598fe055da73420
amrmugl <meet.sagi.here@gmail.com>
2022-04-04 17:01:47 +0500

    strategies implemented

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,17 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../types';
+
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('AT_SECRET'),
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return payload;
+  }
+}
---
+++ src/auth/types/jwtPayloadWithRt.type.ts
@@ -0,0 +1,2 @@
+import { JwtPayload } from './jwtPayload.type';
+export type JwtPayloadWithRt = JwtPayload & { refreshToken: string };
